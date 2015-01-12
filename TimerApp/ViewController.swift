61154c72062a296b976558a5dacbab7cf26889fa
Alexander K <knuxus@gmail.com>
2015-01-12 20:36:09 +0000

    merging

--- TimerApp/ViewController.swift
+++ TimerApp/ViewController.swift
@@ -8,17 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate {
+
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    let pickerData = ["1 Min","2 Min","3 Min","4 Min","5 Min"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        pickerView.dataSource = self
+        pickerView.delegate = self
+       
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    //MARK: - Delegates and data sources
+    //MARK: Data Sources
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    //MARK: Delegates
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return pickerData[row]
+    }
+    
     
 
 }
